e4a61fb5bd257f9f95f9d8dc8b3255f8e34cdce3
Boldi <b.l.jekely@student.rug.nl>
2024-02-28 16:13:35 +0100

    Date import and Machine learning script updated

--- Scripts/data_import.R
+++ Scripts/data_import.R
@@ -1,12 +1,23 @@
-#Merges sample data with info about individuals
+#Merges sample data with info about individuals, and creates Cpg Column
 library(readr)
-Covn <- readRDS("/scratch/s5678684/Results/Cov_ZF_2024_MalesnoW.rds")
-Covn_extract <- readRDS("/scratch/s5678684/Results/Cov_ZF_2024_MalesnoW_Subset.rds")
-info <- read_csv("/scratch/s5678684/Raw_data/Zf_SampleInfo_July2023.csv")
+library(dplyr)
+Covn <- readRDS("/scratch/s5678684/Results/Cov_ZF_2024_MalesnoW.rds")%>%
+  mutate(Sample = as.numeric(Sample))
+
+Covn_extract <- tibble(readRDS("./Results/Cov_ZF_2024_MalesnoW_Subset.rds"))%>%
+  mutate(Sample = as.numeric(Sample))
+
+info <- readr::read_csv("./Raw_data/methyl_male_no_w/Zf_SampleInfo_July2023.csv")
+print("Data loaded")
+
 merge_Covn <- merge(Covn, info, by = "Sample")
 merge_Covn_extract <- merge(Covn_extract, info, by = "Sample")
+print("Data merged")
 
-saveRDS(merged_CovN, "/scratch/s5678684/Results/Cov_ZF_SampleInfo_Feb2024.rds")
-saveRDS(merged_CovN_extract, "/scratch/s5678684/Results/Cov_ZF_SampleInfo_Feb2024_Subset.rds")
+merge_Covn$cpg_site <- paste(merge_Covn$Chromosome, merge_Covn$StartPosition, sep = "_")
+merge_Covn_extract$cpg_site <- paste(merge_Covn_extract$Chromosome, merge_Covn_extract$StartPosition, sep = "_")
+print("Cpg site column created")
 
-
+saveRDS(merge_Covn, "/scratch/s5678684/Results/Cov_ZF_SampleInfo_Feb2024.rds")
+saveRDS(merge_Covn_extract, "./Results/Cov_ZF_SampleInfo_Feb2024_Subset.rds")
+print("Data saved, process finished")
--- Scripts/machine_learning_clock.R
+++ Scripts/machine_learning_clock.R
@@ -1,145 +1,150 @@
 #Set up libraries
-library(tidymodels)
 library(readr)
-library(caret)
 library(tidyverse)
-
+library(caret)
+library(tidymodels)
+library(mice)
 #Import Data
-data <- read_rds("/scratch/s5678684/Results/Cov_ZF_SampleInfo_Feb2024.rds")
+data <- read_rds("Results/Cov_ZF_SampleInfo_Feb2024_Subset.rds")
+#Creating the data frame for modelling, one column with Age at Collection, the remaining Columns with cpg sites with their methylation percentage.
+model_data <- data_frame(data[c("cpg_site", "MethylationPercentage", "AgeCollectionDays")])%>%
+  pivot_wider(names_from = cpg_site, values_from = MethylationPercentage)
+print("Data pivoted")
+#Imputing missing values using the package mice
+#init = mice(model_data, maxit=0)
+#meth = init$method
+#predM = init$predictorMatrix
+#colnames(model_data)
+#predM[, c("AgeCollectionDays")]=0
+#meth[c("AgeCollectionDays")]=""
+#set.seed(123)
+#imputed = mice(model_data, method = meth, predictorMatrix=predM, m=5)
+#print("missing valeus imputed")
 
-#Create a data frame for modelling
-model_data <- data[c("Chromosome", "MethylationPercentage", "BirdID", "AgeCollectionDays")]
-#random seed for model reproductability
-
-
-#Split the data into training (80%) and testing (20%)
-set.seed(123)
-splits <- initial_split(model_data, prop = 0.8, strata = AgeCollectionDays)
-training_data <- training(splits)
-testing_data <- testing(splits)
 #Data transformation: removing cpgs with little variation in TRAINING data
-
-nearzerovar_cpg_list <- nearZeroVar(training_data, freqCut = 85/15, uniqueCut = 50) 
-nearzerovar_training_data <- training_data[, -nearzerovar_cpg_list]
-
-#Data transformation: identifying cpg sites that are highly correlated
-
-highlyCorDescr <- findCorrelation(nearzerovar_training_data, cutoff = 0.8)
-training_data_model <- nearzerovar_training_data[,-highlyCorDescr]
-
-#Data transformation: standardizing and scaling data
-preProcValues <- preProcess(training_data_model, method = c("center", "scale"))
-trainTransformed <- predict(preProcValues, training_data_model)
-
-#Data transformation: removing cpgs with little variation in TESTING data
-nearzerovar_cpg_list2 <- nearZeroVar(testing_data, freqCut = 85/15, uniqueCut = 50) 
-nearzerovar_testing_data <- testing_data[, -nearzerovar_cpg_list2]
+nearzerovar_cpg_list <- nearZeroVar(model_data, freqCut = 85/15, uniqueCut = 50, saveMetrics = FALSE) 
+if (length(nearzerovar_cpg_list) == 0) {
+  nearzerovar_data <- model_data
+} else {
+  nearzerovar_data <- model_data[,-nearzerovar_cpg_list]
+}
+print("Cpg sites with little to no variation removed")
 
 #Data transformation: identifying cpg sites that are highly correlated
-
-highlyCorDescr2 <- findCorrelation(nearzerovar_testing_data, cutoff = 0.8)
-testing_data_model <- nearzerovar_testing_data[,-highlyCorDescr2]
+highlyCorDescr <- findCorrelation(nearzerovar_data, cutoff = 0.8)
+nearzerovar_nocor_data <- nearzerovar_data[,-highlyCorDescr]
+print("highly correlated cpg sites removed")
 
 #Data transformation: standardizing and scaling data
-preProcValues2 <- preProcess(testing_data_model, method = c("center", "scale"))
-testTransformed <- predict(preProcValues2, testing_data_model)
+preProcValues <- preProcess(nearzerovar_nocor_data, method = c("center", "scale"))
+trainTransformed <- predict(preProcValues, nearzerovar_nocor_data)
+print("Data centered on mean 0, std 1")
 
+#Splitting data
+set.seed(123)
+splits <- createDataPartition(trainTransformed$AgeCollectionDays, p = 0.8, list = FALSE, times = 1)
+training_data <- trainTransformed[splits,]
+testing_data <- trainTransformed[-splits,]
+print("Data split")
 #Model tuning
+
 fitControl <- trainControl(method = 'repeatedcv' ,
                            number=10, repeats=10)
 
-ridge_model <- train(AgeCollectionDays ~ ., 
-                     data = trainTransformed, 
-                     method = "glmnet", 
-                     trControl = fitControl, 
-                     tuneGrid = data.frame(
-                     alpha = 0,
-                     lambda = 10^seq(-3, 3, length = 100)), 
-                     tuneLength = 10)
-
 lasso_model <- train(AgeCollectionDays ~ ., 
-                     data = trainTransformed, 
+                     data = training_data, 
                      method = "glmnet", 
                      trControl = fitControl, 
                      tuneGrid = data.frame(
                        alpha = 1,
                        lambda = 10^seq(-3, 3, length = 100)), 
                      tuneLength = 10)
+print("lasso model created")
 
 elastic_model <- train(AgeCollectionDays ~ ., 
-                       data = trainTransformed, 
+                       data = training_data, 
                        method ="glmnet", 
                        trControl = fitControl, 
                        tuneLength = 10)
+print("elastic model created")
 
 elastic_model_05 <- train(AgeCollectionDays ~ ., 
-                          data = trainTransformed,
+                          data = training_data,
                           method = "glmnet", 
                           trControl = fitControl, 
                           tuneGrid = data.frame(
                             alpha = 0.5, 
                             lambda = 10^seq(-3, 3, length = 100)), 
                           tuneLength = 10)
+print("elasti model with alpha 0.5 created")
+
 #Comparing Models
 set.seed(123)
-models_compare <- resamples(list(R=ridge_model, LM=lasso_model, EM=elastic_model, EM05=elastic_model.05))
+models_compare <- resamples(list(LM=lasso_model, EM=elastic_model, EM05=elastic_model_05))
 sink(file = "model_comparison.txt")
 summary(models_compare)
 sink(file = NULL)
+print("model comparison created")
+
 #Model evaluations
 set.seed(123)
-sink(file = "ridge_model.txt")
-predicted_age <- predict.train(ridge_model)
-postResample(pred = predicted_age, trainTransformed$AgeCollectionDays)
-cor.test(predicted_age, trainTransformed$AgeCollectionDays)
-sink(file = NULL)
-
-set.seed(123)
 sink(file = "lasso_model.txt")
+print("Training data")
 predicted.age <- predict.train(lasso_model)
-postResample(pred = predicted.age, trainTransformed$AgeCollectionDays)
-cor.test(predicted.age, trainTransformed$AgeCollectionDays)
+postResample(pred = predicted.age, training_data$AgeCollectionDays)
+cor.test(predicted.age, training_data$AgeCollectionDays)
+print("Testing data")
+predict.lasso.test <- predict(lasso_model, testTransformed)
+postResample(pred = predict.lasso.test, testing_data$AgeCollectionDays)
+cor.test(predict.lasso.test, testing_data$AgeCollectionDays)
 sink(file = NULL)
+print("lasso model_Evaluated")
 
 set.seed(123)
 sink(file = "elastic_model.txt")
+print("Training data")
 predicted.age <- predict.train(elastic_model)
-postResample(pred = predicted.age, trainTransformed$AgeCollectionDays)
-cor.test(predicted.age, trainTransformed$AgeCollectionDays)
+postResample(pred = predicted.age, training_data$AgeCollectionDays)
+cor.test(predicted.age, training_data$AgeCollectionDays)
+print("Testing data")
+predict.elastic.test <- predict(elastic_model, testTransformed)
+postResample(pred = predict.elastic.test, testing_data$AgeCollectionDays)
+cor.test(predict.elastic.test, testing_data$AgeCollectionDays)
 sink(file = NULL)
+print("elastic model evaluated")
 
 set.seed(123)
 sink(file = "elastic_model_05.txt")
+print("Training data")
 predicted.age <- predict.train(elastic_model_05)
-postResample(pred = predicted.age, trainTransformed$AgeCollectionDays)
-cor.test(predicted.age, trainTransformed$AgeCollectionDays)
+postResample(pred = predicted.age, training_data$AgeCollectionDays)
+cor.test(predicted.age, training_data$AgeCollectionDays)
+print("Testing data")
+predict.elastic.test <- predict(elastic_model_05, testTransformed)
+postResample(pred = predict.elastic.test, testing_data$AgeCollectionDays)
+cor.test(predict.elastic.test, testing_data$AgeCollectionDays)
 sink(file = NULL)
+print("elastic model with alpha 0.5 evaluated")
 
-#Testing models on transformed TESTING data
-set.seed(123)
-sink(file = "ridge_testing.txt")
-predict_ridge_test <- predict(ridge_model, testTransformed)
-postResample(pred = predict.ridge.test, testTransformed$AgeCollectionDays)
-cor.test(predict.ridge.test, testTransformed$AgeCollectionDays)
-sink(file = NULL)
-
+#Testing models on TESTING data
 set.seed(123)
 sink(file = "lasso_testing.txt")
 predict_ridge_test <- predict(lasso_model, testTransformed)
-postResample(pred = predict.ridge.test, testTransformed$AgeCollectionDays)
-cor.test(predict.ridge.test, testTransformed$AgeCollectionDays)
+postResample(pred = predict.ridge.test, testing_data$AgeCollectionDays)
+cor.test(predict.ridge.test, testing_data$AgeCollectionDays)
 sink(file = NULL)
 
 set.seed(123)
 sink(file = "elastic_testing.txt")
 predict_ridge_test <- predict(elastic_model, testTransformed)
-postResample(pred = predict.ridge.test, testTransformed$AgeCollectionDays)
-cor.test(predict.ridge.test, testTransformed$AgeCollectionDays)
+postResample(pred = predict.ridge.test, testing_data$AgeCollectionDays)
+cor.test(predict.ridge.test, testing_data$AgeCollectionDays)
 sink(file = NULL)
 
 set.seed(123)
 sink(file = "elastic05_testing.txt")
 predict_ridge_test <- predict(elastic_model_05, testTransformed)
-postResample(pred = predict.ridge.test, testTransformed$AgeCollectionDays)
-cor.test(predict.ridge.test, testTransformed$AgeCollectionDays)
+postResample(pred = predict.ridge.test, testing_data$AgeCollectionDays)
+cor.test(predict.ridge.test, testing_data$AgeCollectionDays)
 sink(file = NULL)
+print("models tested")
